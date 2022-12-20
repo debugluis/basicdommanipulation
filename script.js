4e52733340afdeee583b53c3b3ef9ffbcff731a6
mrluisperez <lkperezospino@gmail.com>
2022-12-19 20:35:10 -0600

    add event listener

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
+const form = document.getElementById('formCalc');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const btnCalc = document.getElementById('btnCalc');
+const result = document.getElementById('result');
+console.log({formCalc, input1, input2, btnCalc, result});
 
-console.log(input.value);
-console.log({h1, p, parrafito, pid, input});
+form.addEventListener('submit', sumarInputValues);
+
+function sumarInputValues(event) {
+    console.log({event});
+    event.preventDefault();
+    var suma = Number(input1.value) + Number(input2.value);
+    console.log(suma);
+    result.innerText = ('Resultado de la suma: '+ suma);
+}
